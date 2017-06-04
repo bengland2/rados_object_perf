e8c4f92288bc368b39fa7a3d08d7f527118bae0c
Ben England <bengland@redhat.com>
2017-06-04 09:24:02 -0400

    add writeread option to test reads of large rados omaps

--- rados-omap.py
+++ rados-omap.py
@@ -11,8 +11,9 @@ def usage(msg):
   print('  --value-size unsigned ')
   print('  --keys-per-call unsigned ')
   print('  --keys unsigned ')
-  print('  --direction read|write')
+  print('  --direction read|write|writeread')
   print('  --pool-name my-pool')
+  print('  --debug Y|(N)')
   sys.exit(1)
 
 def parse_int(valstr, inttype):
@@ -29,8 +30,9 @@ keys_per_call = 1
 value_size = 0
 total_keys = 10
 direction = 'write'
-pool_name = 'omap-test'
+pool_name = 'rbd'
 obj_name = 'my-omap-object'
+debug = False
 
 k=1
 argc = len(sys.argv)
@@ -60,11 +62,16 @@ while k < argc:
     except ValueError as e:
       print('error parsing --%s: %s' % (prmname, e))
   elif prmname == 'direction':
-    if prmval != 'read' and prmval != 'write':
-      usage('--direction can be either set to "read" or "write"')
+    if prmval != 'read' and prmval != 'write' and prmval != 'writeread':
+      usage('--direction can be either set to "read" or "write" or "readwrite"')
     direction = prmval
   elif prmname == 'pool-name':
     pool_name = prmval
+  elif prmname == 'debug':
+    if prmval == 'y' or prmval == 'Y':
+      debug = True
+    else:
+      debug = False
   else:
     usage('--%s: unrecognized parameter name' % prmname)
 
@@ -78,7 +85,7 @@ conn = rados.Rados(conffile='/etc/ceph/ceph.conf')
 conn.connect()
 ioctx = conn.open_ioctx(pool_name)
 
-if direction == 'write':
+if direction == 'write' or direction == 'writeread':
   try:
     ioctx.remove_object(obj_name)
   except rados.ObjectNotFound:
@@ -87,6 +94,7 @@ if direction == 'write':
   ioctx.write_full(obj_name, 'hi there')
 
   start_time = time.time()
+  next_power_of_4 = 4
   base_key = 0
   value = b''
   while base_key < total_keys:
@@ -95,8 +103,30 @@ if direction == 'write':
         omap_key_name = 'key-%09d' % (k + base_key)
         ioctx.set_omap(op, (omap_key_name,), (value,))
       ioctx.operate_write_op(op, obj_name)
-    base_key += keys_per_call
+      base_key += keys_per_call
 
+      # we read the entire omap when it reaches 4^k in size
+      # this means amortized cost of omap read should be O(N) 
+
+      if direction == 'writeread' and base_key > next_power_of_4:
+        if debug: print('next_power_of_4: %d' % next_power_of_4)
+        next_power_of_4 *= 4
+        read_start_time = time.time()
+        read_keycount = 0
+        with rados.ReadOpCtx() as read_op:
+          read_omap, ret = ioctx.get_omap_vals(read_op, "", "", -1)
+          ioctx.operate_read_op(read_op, obj_name)
+          read_keys = (k for k, __ in read_omap)
+          read_keycount = 0
+          for k in read_keys:
+            read_keycount += 1
+            if debug: print(k)
+          read_end_time = time.time()
+          print('keycount = %d' % read_keycount)
+          read_delta_time = read_end_time - read_start_time
+          print('elapsed read time: %f' % read_delta_time)
+          read_throughput = read_keycount / read_delta_time
+          print('read throughput = %f' % read_throughput)
 else: 
   print(ioctx.read(obj_name))
   start_time = time.time()
