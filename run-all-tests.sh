020f5240f3b52877a306275f87f0f7f7872e7e4c
Ben England <bengland@redhat.com>
2017-08-18 12:49:03 -0400

    clear out pbench at start, encode timestamp in directory name

--- run-all-tests.sh
+++ run-all-tests.sh
@@ -3,26 +3,30 @@
 pool=radosperftest
 pgcount=512
 kvpairs_per_call=256
-threads=2
+threads=12
 logtarget=/var/www/html/pub/bluestore-results
-total_spc_GiB=1
-total_omap_spc_GiB=1 
+total_spc_GiB=2
+total_omap_spc_GiB=1
 
 # should not have to edit below here
 
 (( bytes_per_GiB = 1024 * 1024 * 1024 ))
 
+rm -rf /var/lib/pbench-agent/pbench-user-benchmark*
+mkdir -pv $logtarget
+
 # run test in $1 with pbench and archive logs from test 
 # inside pbench result dir, and name result dir $2
 
 pb() {
+  pbench-kill-tools
   pbench-user-benchmark -- "$1"
   pbd=`ls -d /var/lib/pbench-agent/pbench-user-benchmark*`
   if [ -n "$pbd" -a -d "$pbd" ] ; then
     linkval=`readlink rados_logs/latest`
     latestdir=`basename $linkval`
     mv -v rados_logs/$latestdir $pbd/
-    mv -v $pbd $logtarget/$2.latestdir
+    mv -v $pbd $logtarget/$2.$latestdir
   else
     echo "failed to output pbench directory $pbd"
   fi
@@ -34,19 +38,18 @@ ceph osd pool create $pool $pgcount $pgcount
 sleep 5
 
 (( total_spc_bytes = total_spc_GiB * bytes_per_GiB ))
-#for sz in 4096 131072 4194304 ; do
-for sz in 4194304 ; do
+for sz in 4096 131072 4194304 ; do
   (( count = $total_spc_bytes / $sz ))
-  pb "./rados-obj-perf.sh --obj-size $sz --obj-count $count --request-type write --adjust-think-time false" obj.write.sz.$sz.cnt.$count
-  pb "./rados-obj-perf.sh --obj-size $sz --obj-count $count --request-type read --adjust-think-time false" obj.read.sz.$sz.cnt.$count
+  pb "./rados-obj-perf.sh --obj-size $sz --obj-count $count --request-type write --threads $threads --adjust-think-time true" obj.write.sz.$sz.cnt.$count
+  pb "./rados-obj-perf.sh --obj-size $sz --obj-count $count --request-type read --threads $threads --adjust-think-time true" obj.read.sz.$sz.cnt.$count
 done
 #for kcount in 64 1024 ; do
-for kcount in 1024 ; do
+for kcount in 16 1024 65536 ; do
   (( count = $kcount * 1024 ))
   (( kvsize = $total_omap_spc_GiB * $bytes_per_GiB / count ))
   cmd="./rados-obj-perf.sh"
   cmd="$cmd --omap-key-count $count --omap-value-size $kvsize --omap-kvpairs-per-call $kvpairs_per_call"
-  cmd="$cmd --adjust-think-time true "
+  cmd="$cmd --threads $threads --adjust-think-time true "
   pb "$cmd --request-type omap-write" omap.write.sz.$kvsize.cnt.$count.kvpc.$kvpairs_per_call
   pb "$cmd --request-type omap-read"  omap.read.sz.$kvsize.cnt.$count.kvpc.$kvpairs_per_call
 done
